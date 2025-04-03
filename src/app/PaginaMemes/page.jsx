8b7ff2e006546e1923e108610f5520aa5128d1bf
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-04-03 14:49:03 -0300

    feat: create Hero section components

--- src/app/PaginaMemes/page.jsx
+++ src/app/PaginaMemes/page.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import styles from "./memes.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
-
+import HeroSection from "../components/herosection";
 
 export default function PaginaMemes() {
   return (
     <div className={styles.container}>
       <Header />
+      <div className={styles.mainContent}>
+        <div className={styles.contentArea}>
+            <HeroSection />
+        </div>    
+      </div>
       <Footer />
     </div>
   );
